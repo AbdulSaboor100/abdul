dad1272f514c5a80a1b81ea64ab3824785aedf7c
AbdulSaboor100 <70238888+AbdulSaboor100@users.noreply.github.com>
2020-10-27 05:26:46 -0700

    thanks

---
+++ Result GEt/script.js
@@ -0,0 +1,10 @@
+function result(){
+    let math = document.getElementById('math').value;
+    let urdu = document.getElementById('urdu').value
+    let science = document.getElementById('science').value
+    let computer = document.getElementById('computer').value
+    let physics = document.getElementById('physics').value
+    let total =  30 + 40 + 50 + 60 + 70/100;
+    let aa = total*400
+    alert(aa)
+};
